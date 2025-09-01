17d12080eede99074027c5d09ddbfc7ea4cd3424
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-09-01 17:42:45 +0300

    feat(api): add Product list and detail serializers

---
+++ api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Product
+
+
+class ProductListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id", "name", "slug", "image", "price"]
+
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id", "name", "description", "slug", "image", "price"]
