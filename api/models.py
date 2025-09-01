f48f164622085febee6a08a123e22146bfdeaf19
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-09-01 17:26:03 +0300

    Add Category Model

--- api/models.py
+++ api/models.py
@@ -10,3 +10,12 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+    image = models.ImageField(upload_to="categories_img", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
