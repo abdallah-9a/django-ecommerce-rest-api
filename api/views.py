9ffaf865891f4e32ac926926eebb9d891e06f77a
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-09-02 07:36:51 +0300

    Add product_list view and url

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProductListSerializer, ProductDetailSerializer
+from .models import Product
 
 # Create your views here.
+
+@api_view(["GET"])
+def product_list(requst):
+    products = Product.objects.all()
+    serializer = ProductListSerializer(products, many=True)
+    return Response(serializer.data)
