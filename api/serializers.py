618396e026eef83d775cf50ed28ff78758c025bd
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-09-02 10:41:13 +0300

    Add Cart serializer and Cart Item serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product, Category
+from .models import Product, Category, Cart, CartItem
 
 
 class ProductListSerializer(serializers.ModelSerializer):
@@ -21,7 +21,35 @@ class CategoryListSerializer(serializers.ModelSerializer):
 
 
 class CategoryDetailSerailizer(serializers.ModelSerializer):
-    products = ProductListSerializer(read_only=True ,many=True)
+    products = ProductListSerializer(read_only=True, many=True)
+
     class Meta:
         model = Category
         fields = ["id", "name", "image", "products"]
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductListSerializer(read_only=True)
+    sub_total = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CartItem
+        fields = ["id", "product", "quantity", "sub_total"]
+
+    def get_sub_total(self, cartitem):
+        total = cartitem.quantity * cartitem.product.price
+        return total
+
+
+class CartSerializer(serializers.ModelSerializer):
+    cart_items = CartItemSerializer(read_only=True, many=True)
+    cart_total = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Cart
+        fields = ["id", "cart_code", "cart_items", "cart_total"]
+
+    def get_cart_total(self, cart):
+        items = cart.cart_items.all()
+        total = sum([item.product.price * item.quantity for item in items])
+        return total
