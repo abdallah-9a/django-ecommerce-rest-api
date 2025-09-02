9df6e00a6fec7ae64413249462b2c583b1048af4
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-09-02 08:17:15 +0300

    Add category_list view and url

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("products/", views.product_list, name = "product_list"),
-    path("products/<slug:slug>/", views.product_detail, name = "product_detail"),
+    path("products/", views.product_list, name="product_list"),
+    path("products/<slug:slug>/", views.product_detail, name="product_detail"),
+    path("categories/", views.category_list, name="category_list"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import ProductListSerializer, ProductDetailSerializer
-from .models import Product
+from .serializers import (
+    ProductListSerializer,
+    ProductDetailSerializer,
+    CategoryDetailSerailizer,
+    CategoryListSerializer,
+)
+from .models import Product, Category
 
 # Create your views here.
 
@@ -18,4 +23,11 @@ def product_list(requst):
 def product_detail(request, slug):
     product = Product.objects.get(slug=slug)
     serializer = ProductDetailSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def category_list(request):
+    category_list = Category.objects.all()
+    serializer = CategoryListSerializer(category_list, many=True)
     return Response(serializer.data)
