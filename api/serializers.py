96e0d70c2ded58d0cd4d190e7e8edd997aad3d82
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-09-03 06:32:59 +0300

    Add User serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
 from .models import Product, Category, Cart, CartItem
+from django.contrib.auth import get_user_model
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["id", "first_name", "last_name", "email", "profile_picture_url"]
 
 
 class ProductListSerializer(serializers.ModelSerializer):
