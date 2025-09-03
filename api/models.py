302a51837a884c678ef5acea1fe1ad75aecbcfd8
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-09-03 09:13:27 +0300

    Add Wishlist model

--- api/models.py
+++ api/models.py
@@ -113,12 +113,25 @@ class Review(models.Model):
             "product",
         ]  # each user can review only one for each product
         ordering = ["-created_at"]  # LIFO
-        
+
 
 class ProductRating(models.Model):
-    product = models.OneToOneField(Product, on_delete=models.CASCADE, related_name="rating")
+    product = models.OneToOneField(
+        Product, on_delete=models.CASCADE, related_name="rating"
+    )
     avg_rating = models.FloatField(default=0.0)
     total_reviews = models.PositiveIntegerField(default=0)
-    
+
     def __str__(self):
         return f"{self.product.name} - {self.avg_rating} ({self.total_reviews})"
+
+
+class Wishlist(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="wishlist"
+    )
+    products = models.ManyToManyField(Product, related_name="wishlist", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Wishlist"
