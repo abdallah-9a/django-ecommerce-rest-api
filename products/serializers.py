528b055b60e6d4d9128dad94e6ab149d542933bf
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-09-17 13:54:13 +0300

    Add List and create Endpoints for Product and category

---
+++ products/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Product, Category
+
+
+class ProductListSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(
+        queryset=Category.objects.all(), slug_field="name"
+    )
+    image = serializers.ImageField(read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ["id", "name", "slug", "category", "price", "image"]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+    image = serializers.ImageField(read_only=True)
+
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name"]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Product, Category
+from .serializers import ProductListSerializer, CategorySerializer
+from common.pagination import CustomePagination
+from common.permissions import IsAdminOrReadOnly
 
 # Create your views here.
+
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductListSerializer
+    permission_classes = [IsAdminOrReadOnly]
+    pagination_class = CustomePagination
+
+
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = CustomePagination
+    permission_classes = [IsAdminOrReadOnly]
