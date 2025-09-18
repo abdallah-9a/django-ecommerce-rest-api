b151b2f74506e6da979a3c2cf4ff19ce7fef6fef
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-09-18 07:55:05 +0300

    Add Cart and Cart Item model

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "users",
+    "cart",
     "products",
     "rest_framework",
     "rest_framework_simplejwt",
